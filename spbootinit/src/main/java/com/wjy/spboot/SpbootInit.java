5348be358dc790e7c7f0a2bef028c6b5f39b97f0
bozhi <castiel0416@hotmail.com>
2019-01-03 22:08:48 +0800

    提交

--- spbootinit/src/main/java/com/wjy/spboot/SpbootInit.java
+++ spbootinit/src/main/java/com/wjy/spboot/SpbootInit.java
@@ -10,8 +10,12 @@
  */
 package com.wjy.spboot;
 
+import com.wjy.spboot.servlet.SecondServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 〈一句话功能简述〉<br> 
@@ -22,9 +26,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @SpringBootApplication
+@ServletComponentScan
 public class SpbootInit {
 
     public static void main(String[] args) {
         SpringApplication.run(SpbootInit.class,args);
+    }
+    @Bean
+    public ServletRegistrationBean getBean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean();
+        bean.setServlet(new SecondServlet());
+        bean.addUrlMappings("/second");
+        return bean;
     }
 }
