3100aed7a523784ccebdd0e7ee47bf1096e38d79
bozhi <bozhi@suneee.com>
2019-01-04 16:26:36 +0800

    Merge branch 'master' of https://github.com/wjy030/spboot-ler
    # Conflicts:
    #	spbootinit/src/main/java/com/wjy/spboot/model/User.java
    #	spbootinit/src/main/resources/application.yml

---
+++ spbootfree/src/main/java/com/wjy/freem/spbootfree/SpbootfreeApplication.java
@@ -0,0 +1,14 @@
+package com.wjy.freem.spbootfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootfreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbootfreeApplication.class, args);
+    }
+
+}
+
